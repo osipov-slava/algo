32d6e1955784223e5aabbfa133cf87b0d32d6ea7
Slava Osipov <skybright@mail.ru>
2024-08-30 01:19:45 +0300

    task#504974 Depth First Search

--- src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
+++ src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
@@ -1,9 +1,6 @@
 package ru.job4j.newcoll.tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class TreeUtils<T> {
 
@@ -53,6 +50,95 @@ public class TreeUtils<T> {
             current.getChildren().forEach(n -> queue.add(n));
         }
         return list;
+    }
+
+    /**
+     * Метод обходит дерево root и добавляет к узлу с ключом parent
+     * новый узел с ключом child, при этом на момент добавления узел с ключом parent
+     * уже должен существовать в дереве, а узла с ключом child в дереве быть не должно
+     *
+     * @param root   корень дерева
+     * @param parent ключ узла-родителя
+     * @param child  ключ узла-потомка
+     * @return true если добавление произошло успешно и false в обратном случае.
+     * @throws IllegalArgumentException если root является null
+     */
+    public boolean add(Node<T> root, T parent, T child) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Queue<Node<T>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node<T> current = queue.poll();
+            if (current.getValue().equals(parent)) {
+                List<T> listValues = current.getChildren().stream().map(Node::getValue).toList();
+                if (!listValues.contains(child)) {
+                    current.getChildren().add(new Node<>(child));
+                    return true;
+                }
+            }
+            current.getChildren().forEach(n -> queue.add(n));
+        }
+        return false;
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key
+     *
+     * @param root корень дерева
+     * @param key  ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    public Optional<Node<T>> findByKey(Node<T> root, T key) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Deque<Node<T>> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node<T> current = stack.pop();
+            if (current.getValue().equals(key)) {
+                return Optional.of(current);
+            }
+            current.getChildren().forEach(n -> stack.push(n));
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key,
+     * при этом из дерева root удаляется все поддерево найденного узла
+     *
+     * @param root корень дерева
+     * @param key  ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    public Optional<Node<T>> divideByKey(Node<T> root, T key) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        if (root.getValue().equals(key)) {
+            return Optional.of(root);
+        }
+
+        Queue<Node<T>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node<T> current = queue.poll();
+            for (Node<T> node : current.getChildren()) {
+                if (node.getValue().equals(key)) {
+                    current.getChildren().remove(node);
+                    return Optional.of(node);
+                }
+            }
+            current.getChildren().forEach(n -> queue.add(n));
+        }
+        return Optional.empty();
     }
 
 }
